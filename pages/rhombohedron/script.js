e1de3d011084d2b5bde5b516b6e79a405addd72c
Gustavo Sales <gussales14@gmail.com>
2024-04-15 21:18:57 -0300

    área do romboedro concluida

---
+++ pages/rhombohedron/script.js
@@ -0,0 +1,14 @@
+function calculate() {
+  const firstFace = Number(document.getElementById('first_face').value)
+  const secondFace = Number(document.getElementById('second_face').value)
+  const thirstFace = Number(document.getElementById('thirst_face').value)
+  const fourFace = Number(document.getElementById('four_face').value)
+  const fifthFace = Number(document.getElementById('fifth_face').value)
+  const sixthFace = Number(document.getElementById('sixth_face').value)
+
+  alert(`A área desse romboedro é ${fifthFace + secondFace + thirstFace + fourFace + firstFace + sixthFace}`)
+}
+
+
+
+
